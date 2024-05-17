2c055ce4e4366cc247214cc4966585dbb4769be2
Mohammad40244 <152492819+Mohammad40244@users.noreply.github.com>
2024-05-17 19:15:08 +0330

    tedadvoroodi

--- manager.py
+++ manager.py
@@ -26,6 +26,36 @@ def main():
             # Create mode info file
             with open(mode_info_file, "w") as file:
                 file.write("Mode information of the system")
+                
+def activate_account(user, command):
+    if command=="activate":
+        user.is_active = True
+        print(f"Account for {user.username} has been activated.")
+    else:
+        with open("manba.txt", 'r') as file:
+            lines = file.readlines()
+            new_lines = []
+            for line in lines:
+                elements = line.split()
+                if len(elements) >= 4:
+                    if elements[0]==user:
+                        elements[3] = 'F'  # Replace 'new_value' with the desired value
+                        new_line = ' '.join(elements)
+                        new_lines.append(new_line)
+                    else:
+                        new_lines.append(line)
+        with open('manba.txt', 'w') as file:
+            for line in new_lines:
+                file.write(f"{line}\n")
+        
+        user.is_activate = False
+    
+def eliminate():
+    # Open the file in write mode to empty its contents
+    with open('projects.txt', 'w'):
+        pass
+    with open('manba.txt', 'w'):
+        pass
 
 if __name__ == "__main__":
     main()
